dfb65f08e9d16feb933b845ef8ded70531784185
Jorge Marciel Pariente <jorgemarciel15@gmail.com>
2024-05-27 14:13:31 +0200

    Initialized repository

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Jorge Marciel on 27/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
